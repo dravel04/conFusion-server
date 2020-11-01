461555ae6a9a572f0b8186bf782d2ef9352f173e
Rafael Antonio Marin Sanchez <rafamasa91@gmail.com>
2020-11-02 00:01:30 +0100

    Admin can get a report of all users on localhost/users

--- routes/leaderRouter.js
+++ routes/leaderRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var authenticate = require('../authenticate');
 
 const Leaders = require('../models/leaders');
 
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var passport = require('passport');
 var router = express.Router();
 const bodyParser = require('body-parser');
+
 var User = require('../models/user');
 var authenticate = require('../authenticate');
 
@@ -46,21 +47,19 @@ router.post('/login', passport.authenticate('local'), (req, res) => { // If can'
 });
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function (req, res, next) {
+  User.find({})
+    .then((users) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(users);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 });
 
 router.get('/logout', (req, res) => {
-  if (req.session) {
-    req.session.destroy();
-    res.clearCookie('session-id');
-    res.redirect('/');
-  }
-  else {
-    var err = new Error('You are not logged in!');
-    err.status = 403;
-    next(err);
-  }
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
