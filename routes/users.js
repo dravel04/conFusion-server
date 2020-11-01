3a20bb08c36770c167996ea8f3545c44a7a16a01
Rafael Antonio Marin Sanchez <rafamasa91@gmail.com>
2020-11-01 21:36:54 +0100

    Add mongoose population for dishes comments

--- routes/users.js
+++ routes/users.js
@@ -17,20 +17,33 @@ router.post('/signup', (req, res) => {
         res.json({ err: err });
       }
       else {
-        passport.authenticate('local')(req, res, () => {    // To finish registration, check if the 
-          res.statusCode = 200;                             // new User can be authenticated correctly
-          res.setHeader('Content-Type', 'application/json');
-          res.json({ success: true, status: 'Registration Successful!' })
-        })
+        if (req.body.firstname) {
+          user.firstname = req.body.firstname;
+          if (req.body.lastname)
+            user.lastname = req.body.lastname
+        }
+        user.save((err, user) => {
+          if (err) {
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({ err: err });
+            return;
+          }
+          passport.authenticate('local')(req, res, () => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({ success: true, status: 'Registration Successful!' })
+          });
+        });
       }
-    })
-})
+    });
+});
 
 router.post('/login', passport.authenticate('local'), (req, res) => { // If can't call passport.authenticate('local')
   res.statusCode = 200;                                               // send a error to client and abort login       
-  var token = authenticate.getToken({_id: req.user._id}); // when login 1st time correctly, create the token
+  var token = authenticate.getToken({ _id: req.user._id }); // when login 1st time correctly, create the token
   res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, token: token, status: 'You are successfully logged in!'}); //and send back inside request
+  res.json({ success: true, token: token, status: 'You are successfully logged in!' }); //and send back inside request
 });
 
 /* GET users listing. */
